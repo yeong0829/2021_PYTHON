d1aee04095a0f408a1f1e5840be0d6a6152e1eec
주서영 <w2017@e-mtim.hs.kr>
2021-08-27 10:18:45 +0900

    package

--- module, package/do.py
+++ module, package/do.py
@@ -1,3 +1,31 @@
 import my_module as mm
 mm.지원이()
 print(mm.date)
+
+
+import travle.gangreung as ga
+gp = ga.GangreungPackage()
+print(gp)
+
+from travle import gangreung
+gp = gangreung.GangreungPackage()
+print(gp)
+
+from travle.gangreung import GangreungPackage
+gp = GangreungPackage()
+print(gp)
+
+
+from travle import ilsan
+ilsan.이지()
+
+from travle.ilsan import 이지
+이지()
+
+import travle.ilsan
+travle.ilsan.이지()
+
+from travle import *
+ilsan.이지()
+gp = gangreung.GangreungPackage()
+print(gp)
---
+++ module, package/travle/ilsan.py
@@ -0,0 +1,5 @@
+def 이지():
+    print('아쿠아리움')
+
+if __name__ == '__main__':  # 자신을 run 할때는 실행d, import 되었을때는 실행x
+    이지()
