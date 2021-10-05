6a6ebacbe317c9f6673176819e8ac58f656023ef
yeong431 <w2017@e-mtim.hs.kr>
2021-10-05 11:27:03 +0900

    load text, read(), readline(), readlines()

---
+++ file handler/load_text.py
@@ -0,0 +1,32 @@
+# f = open('text.txt', 'r', encoding='utf-8')
+#
+# data = f.read()
+#
+# f.close()
+#
+# print(data)
+
+
+print('한꺼번에 전체 읽기')
+# f = open('text.txt', 'r', encoding='utf-8')
+# data = f.read()
+# f.close()
+# print(data)
+with open('text.txt', 'r', encoding='utf-8') as f:
+    data = f.read()
+print(data)
+
+print('한 줄씩 읽기')
+with open('text.txt', 'r', encoding='utf-8') as f:
+    while True:
+        line = f.readline()  #line: 'hello\n'
+        if line == '':       #파일에서 읽어올 내용이 없으면 멈추기
+            break
+        print(line.rstrip())
+
+print('한꺼번에 전체 읽어서, 한줄씩 리스트')
+with open('text.txt', 'r', encoding='utf-8') as f:
+    lines = f.readline()
+    #print(lines)
+for line in lines:
+    print(line.rstrip())
